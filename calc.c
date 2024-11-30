e7d7f6ee08c8c58247d7271b3f7d383490aa232a
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-30 18:08:55 +0200

    added square root and fixed a bug

--- calc.c
+++ calc.c
@@ -10,49 +10,62 @@ int main(void)
     printf("Please enter math Operator.\n Addition with +\n Subtraction with -\n Multipling with *\n Dividing with /\n Square Root with S\n ");
     scanf("%c", &oper);
 
-    //Adding square root
+//Adding square root
 
-    if (oper == 'S', 's')
-    {
-        printf("Enter number: ");
-        scanf("%lf", &a);
-        printf("The square root is: %.2lf\n", sqrt(a));
-        return 2;
-    }
+
 
 // initiate error if another character chosen
 // make conditions for each operation
 
-    printf("first number: ");
-    scanf("%lf", &a);
-    printf("second number: ");
-    scanf("%lf", &b);
     
     if (oper == '+')
     {
+        printf("first number: ");
+        scanf("%lf", &a);
+        printf("second number: ");
+        scanf("%lf", &b);
         c = a + b;
         printf("%.2lf\n", c);
     }
 
     else if (oper == '-')
     {
+        printf("first number: ");
+        scanf("%lf", &a);
+        printf("second number: ");
+        scanf("%lf", &b);
         c = a - b;
         printf("%.2lf\n", c);
     }
 
     else if (oper == '*')
     {
+        printf("first number: ");
+        scanf("%lf", &a);
+        printf("second number: ");
+        scanf("%lf", &b);
         c = a * b;
         printf("%.2lf\n", c);
     }
     
     else if (oper == '/')
     {
+        printf("first number: ");
+        scanf("%lf", &a);
+        printf("second number: ");
+        scanf("%lf", &b);
         c = a / b;
         printf("%.2lf\n", c);
     }
+
+    else if (oper == 'S', 's')
+    {
+        printf("Enter number: ");
+        scanf("%lf", &a);
+        printf("The square root is: %.2lf\n", sqrt(a));
+    }
     
-    else if (oper != '/', '+', '-', '*')
+    else if (oper != '/', '+', '-', '*', 's', 'S')
     {
         printf("invalid\n");
         return 1;
