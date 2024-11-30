6eec320727b53d96ccfe105b56acd94ac7fbbcb5
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-30 13:26:35 +0200

    added square root

--- calc.c
+++ calc.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h> 
 
 char oper;
@@ -6,8 +7,18 @@ double a, b, c;
 
 int main(void)
 {
-    printf("Please enter math Operator.\n Addition with +\n Subtraction with -\n Multipling with *\n Dividing with /\n");
+    printf("Please enter math Operator.\n Addition with +\n Subtraction with -\n Multipling with *\n Dividing with /\n Square Root with S\n ");
     scanf("%c", &oper);
+
+    //Adding square root
+
+    if (oper == 'S', 's')
+    {
+        printf("Enter number: ");
+        scanf("%lf", &a);
+        printf("The square root is: %.2lf\n", sqrt(a));
+        return 2;
+    }
 
 // initiate error if another character chosen
 // make conditions for each operation
@@ -46,4 +57,5 @@ int main(void)
         printf("invalid\n");
         return 1;
     }
+
 }
