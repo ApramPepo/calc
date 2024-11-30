cc7758f57864ae573b8176df1132504e055ff979
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-30 11:50:05 +0200

    fixing else if

--- calc.c
+++ calc.c
@@ -9,24 +9,32 @@ int main(void)
     printf("Please enter math Operator.\n Addition with +\n Subtraction with -\n Multipling with *\n Dividing with /\n");
     scanf("%c", &oper);
 
-// initiate error, if another character chosen
-
-   if(oper != '+', '-', '*', '/')
-    {
-        printf("Invalid\n");
-        return 1;
-    }
+// initiate error if another character chosen
 
     printf("first number: ");
     scanf("%lf", &a);
     printf("second number: ");
     scanf("%lf", &b);
-
- 
+    
     if (oper == '+');
     {
         c = a + b;
         printf("%lf", c);
+    } else if (oper == '-')
+    {
+        c = a - b;
+        printf("%lf", c);
+    } else if (oper == '*')
+    {
+        c = a * b;
+        printf("%lf", c);
+    } else if (oper == '/')
+    {
+        c = a / b;
+        printf("%lf", c);
+    } else
+    {
+        printf("invalid\n");
+        return 1;
     }
-    
 }
